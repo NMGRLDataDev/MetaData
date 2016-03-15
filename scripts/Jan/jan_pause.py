7e8f6450b5687311cfdb1ae530e813620b45d9cc
Jake Ross <jirhiker@gmail.com>
2016-03-14 21:16:23 -0600

    repo updated for analysis 19211-01

---
+++ scripts/Jan/jan_pause.py
@@ -0,0 +1,18 @@
+#===============================================================================
+# EXTRACTION SCRIPT jan_pause.py
+#===============================================================================
+'''
+eqtime: 5
+'''
+
+
+def main():
+    
+    info('pause script')
+    begin_interval(duration)
+    sleep(duration/2.0)
+    
+    complete_interval()
+    
+    gosub('jan:PrepareForAirShot')
+    #gosub('jan:WaitForMiniboneAccess')
