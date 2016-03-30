f5b4e2ad47a302791f62e91cbe2131c38e69ee1b
jirhiker <jirhiker@gmail.com>
2016-03-30 15:01:57 -0600

    repo updated for analysis ba-01-J-1654

--- scripts/Jan/jan_pause.py
+++ scripts/Jan/jan_pause.py
@@ -2,17 +2,30 @@
 # EXTRACTION SCRIPT jan_pause.py
 #===============================================================================
 '''
-eqtime: 5
 '''
-
-
 def main():
-    
     info('pause script')
-    begin_interval(duration)
-    sleep(duration/2.0)
+    sleep(duration)
+#===============================================================================
+# POST EQUILIBRATION SCRIPT jan_pump_extraction_line.py
+#===============================================================================
+def main():
+    info('Pump after analysis')
+
+    if extract_device=="FusionsDiode":
+        info('Pump after Jan diode analysis')
+        gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
+        gosub('jan:PumpMiniboneAfterDiodeAnalysis')
+    else:
+        gosub('jan:PumpMicrobone')
+        v=get_resource_value(name='JanMiniboneFlag')  
+        info('get resource value {}'.format(v))
+        if get_resource_value(name='JanMiniboneFlag'):
+            gosub('jan:PumpMinibone')
+#===============================================================================
+# POST MEASUREMENT SCRIPT jan_pump_ms.py
+#===============================================================================
+def main():
+    info('Pumping spectrometer')
+    open(name='O')
     
-    complete_interval()
-    
-    gosub('jan:PrepareForAirShot')
-    #gosub('jan:WaitForMiniboneAccess')
