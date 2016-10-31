036963f9e504c9d3c0ad9d6cfd18210e9b360cc1
nmgrluser <jan@argonlab7s-Mac-mini.local>
2016-10-31 10:47:45 -0600

    repo updated for analysis 65321-04A

--- scripts/jan/jan_co2.py
+++ scripts/jan/jan_co2.py
@@ -80,6 +80,22 @@ def do_extraction():
         complete_interval()
 
 #===============================================================================
+# POST EQUILIBRATION SCRIPT jan_pump_extraction_line.py
+#===============================================================================
+def main():
+    info('Pump after analysis')
+
+    if extract_device=="FusionsDiode":
+        info('Pump after Jan diode analysis')
+        gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
+        gosub('jan:PumpMiniboneAfterDiodeAnalysis')
+    else:
+        gosub('jan:PumpMicrobone')
+        v=get_resource_value(name='JanMiniboneFlag')  
+        info('get resource value {}'.format(v))
+        if get_resource_value(name='JanMiniboneFlag'):
+            gosub('jan:PumpMinibone')
+#===============================================================================
 # POST MEASUREMENT SCRIPT jan_pump_ms.py
 #===============================================================================
 def main():
