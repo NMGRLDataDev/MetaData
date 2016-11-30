868650d3d166049db306b6b38b758f9782a26d15
nmgrluser <jan@argonlab7s-Mac-mini.local>
2016-11-30 15:52:45 -0600

    repo updated for analysis ba-01-J-2158

--- scripts/jan/jan_sniffair_x2.py
+++ scripts/jan/jan_sniffair_x2.py
@@ -19,22 +19,6 @@ def main():
     gosub('common:SniffPipette2')
     
 #===============================================================================
-# POST EQUILIBRATION SCRIPT jan_pump_extraction_line.py
-#===============================================================================
-def main():
-    info('Pump after analysis')
-
-    if extract_device=="FusionsDiode":
-        info('Pump after Jan diode analysis')
-        gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
-        gosub('jan:PumpMiniboneAfterDiodeAnalysis')
-    else:
-        gosub('jan:PumpMicrobone')
-        v=get_resource_value(name='JanMiniboneFlag')  
-        info('get resource value {}'.format(v))
-        if get_resource_value(name='JanMiniboneFlag'):
-            gosub('jan:PumpMinibone')
-#===============================================================================
 # POST MEASUREMENT SCRIPT jan_pump_ms.py
 #===============================================================================
 def main():
