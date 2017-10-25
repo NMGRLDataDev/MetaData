6ac9c28bd00d5e688232bde6df91b21aa0bebc57
felix <nmgrluser@gmail.com>
2017-10-24 18:08:36 -0600

    repo updated for analysis bc-02-F-632

---
+++ scripts/felix/felix_cocktail_x2.py
@@ -0,0 +1,44 @@
+#===============================================================================
+# EXTRACTION SCRIPT felix_cocktail_x2.py
+#===============================================================================
+'''
+modifier: 02
+eqtime: 30
+'''
+
+def main():
+    info("Cocktail Pipette x1")
+    gosub('felix:WaitForMiniboneAccess')
+    gosub('felix:PrepareForAirShot')
+
+    #shot 1
+    gosub('common:EvacPipette1')
+    gosub('common:FillPipette1')
+    gosub('felix:PrepareForAirShotExpansion')
+    gosub('common:ExpandPipette1')
+    close(description='Outer Pipette 1')
+    sleep(1)
+    
+    #shots 2
+    for i in range(1):
+        info('Shot {}'.format(i+2))
+        gosub('common:FillPipette1')
+        gosub('common:ExpandPipette1')
+        close(description='Outer Pipette 1')
+        sleep(1)
+    
+#===============================================================================
+# POST EQUILIBRATION SCRIPT felix_pump_extraction_line.py
+#===============================================================================
+def main():
+    info('Pump after analysis')
+    gosub('felix:PumpBone')
+    if get_resource_value(name='FelixMiniboneFlag'):
+        gosub('felix:PumpMinibone')
+#===============================================================================
+# POST MEASUREMENT SCRIPT felix_pump_ms.py
+#===============================================================================
+def main():
+	info('Pumping spectrometer')
+	open(name='V')
+	
