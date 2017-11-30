7ea8b94b19eafb829afabbe11ec5adf6dd97ec2a
nmgrluser <jan@argonlab7s-Mac-mini.local>
2017-11-30 16:09:04 -0600

    repo updated for analysis 4508-01

--- scripts/jan/jan_co2_video.py
+++ scripts/jan/jan_co2_video.py
@@ -12,6 +12,7 @@ def main():
     set_motor('beam',beam_diameter)
 
     cooldown = 2
+    accum = 0
     if analysis_type=='blank':
         info('is blank. not heating')
         '''
@@ -28,22 +29,33 @@ def main():
         this is the most generic what to move and fire the laser
         position is always a list even if only one hole is specified
         '''
-        with video_recording(run_identifier):
-            for i,pi in enumerate(position):
-                '''
-                position the laser at pi, pi can be an holenumber or (x,y)
-                '''
-                move_to_position(pi, autocenter=True)
-                if i==0:
-                    close(description='Microbone to Turbo')
 
-                do_extraction()
-                if disable_between_positions:
-                    end_extract()
-            end_extract()
-            disable()
-            sleep(cooldown)
-    sleep(cleanup-cooldown)
+        # make sure light is on before moving
+        with grain_polygon():
+            with video_recording(run_identifier):
+                for i,pi in enumerate(position):
+                    '''
+                    position the laser at pi, pi can be a holenumber or (x,y)
+                    '''
+                    with lighting():
+                        sleep(2)
+                        accum+=2
+                        move_to_position(pi, autocenter=True)
+                        sleep(2)
+                        accum+=2
+
+                    if i==0:
+                        close(description='Microbone to Turbo')
+
+                    do_extraction()
+
+                    if disable_between_positions:
+                        end_extract()
+                end_extract()
+                disable()
+                sleep(cooldown)
+
+    sleep(max(0,cleanup-cooldown-accum))
 
 
 def do_extraction():
@@ -69,8 +81,8 @@ def do_extraction():
         info('begin interval')
         begin_interval(duration)
 
-        info('set extract to {}'.format(extract_value))
-        extract(extract_value)
+        info('set extract to {}, {}'.format(extract_value, extract_units))
+        extract(extract_value, extract_units)
         #sleep(2)
 
         if pattern:
