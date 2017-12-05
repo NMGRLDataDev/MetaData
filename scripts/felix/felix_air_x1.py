6d99b72a518d735a7a817c8a9bbcbb210e6a974c
felix <nmgrluser@gmail.com>
2017-12-05 15:05:52 -0700

    repo updated for analysis a-02-F-04

--- scripts/felix/felix_air_x1.py
+++ scripts/felix/felix_air_x1.py
@@ -2,7 +2,7 @@
 # EXTRACTION SCRIPT felix_air_x1.py
 #===============================================================================
 '''
-modifier: 01
+modifier: 02
 eqtime: 35
 '''
 
@@ -14,7 +14,7 @@ def main():
     gosub('common:FillPipette2')
     gosub('felix:PrepareForAirShotExpansion')
     gosub('common:ExpandPipette2')
-    close(description='Outer Pipette 2')
+    # close(description='Outer Pipette 2')
     
     
 
