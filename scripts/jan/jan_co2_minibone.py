248a392042ed2aaabbc0e6c5479a832fbd31a73a
nmgrluser <jan@argonlab7s-Mac-mini.local>
2018-02-05 10:28:06 -0600

    repo updated for analysis 66089-03A

--- scripts/jan/jan_co2_minibone.py
+++ scripts/jan/jan_co2_minibone.py
@@ -6,77 +6,106 @@ modifier: 01
 eqtime: 15
 '''
 def main():
-    info('Jan CO22_GP50 laser analysis')
-    gosub('jan:WaitForMiniboneAccess')
+    info('Jan CO2 laser analysis')
+    gosub('jan:WaitForCO2Access')
     gosub('jan:PrepareForCO2_minibone_Analysis')
-
-    set_motor('beam',beam_diameter)
-
-    if analysis_type=='blank':
-        info('is blank. not heating')
-        '''
-        sleep cumulative time to account for blank
-        during a multiple position analysis
-        '''
-        close(description='Microbone to Turbo')
-        numPositions=len(position)
-        
-        sleep(duration*numPositions)
-    else:
-
-        '''
-        this is the most generic what to move and fire the laser
-        position is always a list even if only one hole is specified
-        '''
-        enable()
-        for pi in position:
-            ''' 
-            position the laser at pi, pi can be an holenumber or (x,y)
-            '''
-            move_to_position(pi)
-            sleep(10)
-            close(description='Microbone to Turbo')
-            do_extraction()
-            if disable_between_positions:
-                    end_extract()
-        end_extract()
-        disable()
-        
-    sleep(cleanup)
+    gosub('jan:CO2Analysis')
 
 
-def do_extraction():
-    if ramp_duration>0:
-        '''
-        style 1.
-        '''
-#               begin_interval(duration)
-#               info('ramping to {} at {} {}/s'.format(extract_value, ramp_rate, extract_units)
-#               ramp(setpoint=extract_value, rate=ramp_rate)
-#               complete_interval()
-        '''
-        style 2.
-        '''
-        elapsed=ramp(setpoint=extract_value, duration=ramp_duration)
-        pelapsed=execute_pattern(pattern)
-        sleep(min(0, duration-elapsed-pelapsed))
 
-    else:
-                
-        
-        begin_interval(duration)
-        
-        info('set extract to {}'.format(extract_value))
-        extract(extract_value)
-        sleep(2)
-    
-        if pattern:
-            info('executing pattern {}'.format(pattern))
-            execute_pattern(pattern)
-        
-        complete_interval()
-    
-    
+# '''
+# eqtime: 12
+# '''
+# def main():
+#     info('Jan CO2 laser analysis')
+#     gosub('jan:WaitForCO2Access')
+#     gosub('jan:PrepareForCO2Analysis')
+#
+#     set_motor('beam',beam_diameter)
+#
+#     cooldown = 2
+#     accum = 0
+#     if analysis_type=='blank':
+#         info('is blank. not heating')
+#         '''
+#         sleep cumulative time to account for blank
+#         during a multiple position analysis
+#         '''
+#         close(description='Microbone to Turbo')
+#         numPositions=len(position)
+#
+#         sleep(duration*max(1,numPositions))
+#     else:
+#
+#         '''
+#         this is the most generic what to move and fire the laser
+#         position is always a list even if only one hole is specified
+#         '''
+#
+#         # make sure light is on before moving
+#         with grain_polygon():
+#             with video_recording('{}/{}'.format(load_identifier,run_identifier)):
+#                 for i,pi in enumerate(position):
+#                     '''
+#                     position the laser at pi, pi can be a holenumber or (x,y)
+#                     '''
+#                     with lighting():
+#                         if i>0:
+#                             accum+=2
+#                         sleep(2)
+#
+#                         move_to_position(pi, autocenter=True)
+#                         sleep(2)
+#                         if i>0:
+#                             accum+=2
+#
+#                     if i==0:
+#                         close(description='Microbone to Turbo')
+#
+#                     do_extraction()
+#
+#                     if disable_between_positions:
+#                         end_extract()
+#                 end_extract()
+#                 disable()
+#                 sleep(cooldown)
+#
+#     sleep(max(0,cleanup-cooldown-accum))
+#
+#
+# def do_extraction():
+#     info('enable laser')
+#     enable()
+#     if ramp_duration>0:
+#         '''
+#         style 1.
+#         '''
+# #               begin_interval(duration)
+# #               info('ramping to {} at {} {}/s'.format(extract_value, ramp_rate, extract_units)
+# #               ramp(setpoint=extract_value, duration=ramp_duration)
+# #               complete_interval()
+#         '''
+#         style 2.
+#         '''
+#         elapsed=ramp(setpoint=extract_value, duration=ramp_duration)
+#         pelapsed=execute_pattern(pattern)
+#         sleep(max(0, duration-elapsed-pelapsed))
+#
+#     else:
+#
+#         info('begin interval')
+#         begin_interval(duration)
+#
+#         info('set extract to {}, {}'.format(extract_value, extract_units))
+#         extract(extract_value, extract_units)
+#         #sleep(2)
+#
+#         if pattern:
+#             info('executing pattern {}'.format(pattern))
+#             execute_pattern(pattern)
+#
+#         complete_interval()
+
 #===============================================================================
 # POST EQUILIBRATION SCRIPT jan_pump_extraction_line.py
 #===============================================================================
