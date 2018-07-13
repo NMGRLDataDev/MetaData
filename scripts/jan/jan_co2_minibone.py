a0ab0abf6d86f963266f9c0167d6e83e2d099dfc
nmgrluser <jan@argonlab7s-Mac-mini.local>
2018-07-13 14:06:46 -0600

    repo updated for analysis bu-FC-J-9556

--- scripts/jan/jan_co2_minibone.py
+++ scripts/jan/jan_co2_minibone.py
@@ -8,6 +8,7 @@ eqtime: 15
 def main():
     info('Jan CO2 laser analysis')
     gosub('jan:WaitForCO2Access')
+    gosub('jan:WaitForMiniboneAccess')
     gosub('jan:PrepareForCO2_minibone_Analysis')
     gosub('jan:CO2Analysis')
 
