3e740517d45c76384462e5c1a9f1b6cff21c6036
nmgrluser <jan@argonlab7s-Mac-mini.local>
2018-10-23 15:36:12 -0600

    repo updated for analysis 66408-01C

---
+++ scripts/jan/jan_co2_no_grain_identification.py
@@ -0,0 +1,39 @@
+#===============================================================================
+# EXTRACTION SCRIPT jan_co2_no_grain_identification.py
+#===============================================================================
+'''
+eqtime: 12
+'''
+def main():
+    info('Jan CO2 laser analysis')
+    gosub('jan:WaitForCO2Access')
+    gosub('jan:PrepareForCO2Analysis')
+    gosub('jan:CO2AnalysisNoGrainID')
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT jan_pump_extraction_line.py
+#===============================================================================
+def main():
+    info('Pump after analysis')
+
+    if extract_device=="FusionsDiode":
+        info('Pump after Jan diode analysis')
+        gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
+        gosub('jan:PumpMiniboneAfterDiodeAnalysis')
+    else:
+        gosub('jan:PumpMicrobone')
+        v=get_resource_value(name='JanMiniboneFlag')
+        info('get resource value {}'.format(v))
+        if v:
+            info('Pumping Minibone')
+            gosub('jan:PumpMinibone')
+        else:
+            info('Not Pumping Minibone')
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT jan_pump_ms.py
+#===============================================================================
+def main():
+    info('Pumping spectrometer')
+    open(name='O')
+    
