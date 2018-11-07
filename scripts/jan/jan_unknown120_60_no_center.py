58bd2852a3a76f3414cd0d103334fb03e71bd49b
nmgrluser <jan@argonlab7s-Mac-mini.local>
2018-11-07 08:21:24 -0700

    repo updated for analysis bu-FC-J-9945

--- scripts/jan/jan_unknown120_60_no_center.py
+++ scripts/jan/jan_unknown120_60_no_center.py
@@ -14,12 +14,13 @@ equilibration:
   inlet_delay: 3
   outlet: O
   use_extraction_eqtime: true
+  post_equilibration_delay: 5
 multicollect:
   counts: 120
   detector: H1
   isotope: Ar40
 peakcenter:
-  after: false
+  after: true
   before: false
   detector: H1
   detectors:
@@ -33,20 +34,20 @@ peakhop:
   use_peak_hop: false
 '''
 ACTIVE_DETECTORS=('H2','H1','AX','L1','L2','CDD')
-    
+
 def main():
     info('unknown measurement script')
-    
+
     activate_detectors(*ACTIVE_DETECTORS)
-   
-    
+
+
     if mx.peakcenter.before:
         peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope)
-    
+
     if mx.baseline.before:
         baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector,
                   settling_time=mx.baseline.settling_time)
-    
+
     position_magnet(mx.multicollect.isotope, detector=mx.multicollect.detector)
 
     #sniff the gas during equilibration
@@ -58,19 +59,22 @@ def main():
     Equilibrate is non-blocking so use a sniff or sleep as a placeholder
     e.g sniff(<equilibration_time>) or sleep(<equilibration_time>)
     '''
-    equilibrate(eqtime=eqt, inlet=mx.equilibration.inlet, outlet=mx.equilibration.outlet, 
+    equilibrate(eqtime=eqt, inlet=mx.equilibration.inlet, outlet=mx.equilibration.outlet,
                delay=mx.equilibration.inlet_delay)
     set_time_zero()
-    
-    sniff(eqt)    
+
+    sniff(eqt)
     set_fits()
     set_baseline_fits()
-    
+
+    # delay to migitate 39Ar spike from inlet valve close
+    sleep(mx.equilibration.post_equilibration_delay)
+
     #multicollect on active detectors
     multicollect(ncounts=mx.multicollect.counts, integration_time=1)
-    
+
     if mx.baseline.after:
-        baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector, 
+        baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector,
                   settling_time=mx.baseline.settling_time)
     if mx.peakcenter.after:
         activate_detectors(*mx.peakcenter.detectors, **{'peak_center':True})
@@ -78,7 +82,6 @@ def main():
                     integration_time=mx.peakcenter.integration_time)
 
     if use_cdd_warming:
-       gosub('warm_cdd', argv=(mx.equilibration.outlet,))    
-       
+       gosub('warm_cdd', argv=(mx.equilibration.outlet,))
+
     info('finished measure script')
-    
