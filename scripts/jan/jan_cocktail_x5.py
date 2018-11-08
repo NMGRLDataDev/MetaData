69e3cccf8e1f2f55babf638d9198ec975b917275
nmgrluser <jan@argonlab7s-Mac-mini.local>
2018-11-08 13:44:11 -0700

    repo updated for analysis c-02-J-3969

---
+++ scripts/jan/jan_cocktail_x5.py
@@ -0,0 +1,54 @@
+#===============================================================================
+# EXTRACTION SCRIPT jan_cocktail_x5.py
+#===============================================================================
+'''
+modifier: 02
+'''
+def main():
+    info("Jan Cocktail Pipette x1")
+    gosub('jan:WaitForMiniboneAccess')
+    gosub('jan:PrepareForAirShot')
+    gosub('jan:EvacPipette1')
+    gosub('common:FillPipette1')
+    gosub('jan:PrepareForAirShotExpansion')
+    gosub('common:ExpandPipette1')
+    gosub('common:FillPipette1')
+    gosub('jan:PrepareForAirShotExpansion')
+    gosub('common:ExpandPipette1')
+    gosub('common:FillPipette1')
+    gosub('jan:PrepareForAirShotExpansion')
+    gosub('common:ExpandPipette1')
+    gosub('common:FillPipette1')
+    gosub('jan:PrepareForAirShotExpansion')
+    gosub('common:ExpandPipette1')
+    gosub('common:FillPipette1')
+    gosub('jan:PrepareForAirShotExpansion')
+    gosub('common:ExpandPipette1')
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT jan_pump_extraction_line.py
+#===============================================================================
+def main():
+    info('Pump after analysis')
+
+    if extract_device=="FusionsDiode":
+        info('Pump after Jan diode analysis')
+        gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
+        gosub('jan:PumpMiniboneAfterDiodeAnalysis')
+    else:
+        gosub('jan:PumpMicrobone')
+        v=get_resource_value(name='JanMiniboneFlag')
+        info('get resource value {}'.format(v))
+        if v:
+            info('Pumping Minibone')
+            gosub('jan:PumpMinibone')
+        else:
+            info('Not Pumping Minibone')
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT jan_pump_ms.py
+#===============================================================================
+def main():
+    info('Pumping spectrometer')
+    open(name='O')
+    
