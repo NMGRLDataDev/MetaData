799450ef862ee4468de3e01d419cb4adaacf3954
felix <nmgrluser@gmail.com>
2018-12-19 17:25:53 -0700

    repo updated for analysis 66676-02A

--- scripts/felix/felix_diode.py
+++ scripts/felix/felix_diode.py
@@ -55,36 +55,52 @@ def main():
     
     sleep(cleanup)
 
-
 def do_extraction():
     
-    if ramp_rate>0:
-        '''
-        style 1.
-        '''
-        #               begin_interval(duration)
-        #               info('ramping to {} at {} {}/s'.format(extract_value, ramp_rate, extract_units)
-        #               ramp(setpoint=extract_value, rate=ramp_rate)
-        #               complete_interval()
-        '''
-        style 2.
-        '''
-        elapsed=ramp(setpoint=extract_value, rate=ramp_rate)
-        pelapsed=execute_pattern(pattern)
-        sleep(min(0, duration-elapsed-pelapsed))
-
+    info('begin interval')
+    begin_interval(duration)
+    
+    if ramp_duration>0:
+        info('ramping to {} at {}/s'.format(extract_value, ramp_rate, extract_units))
+        ramp(setpoint=extract_value, duration=ramp_duration, period=0.5)
     else:
-        begin_interval(duration)
+        extract(extract_value, extract_units)
+    
+    if pattern:
+        info('executing pattern {}'.format(pattern))
+        execute_pattern(pattern)
         
-        info('set extract to {} ({})'.format(extract_value, extract_units))
-        extract()
-        sleep(2)
-
-        if pattern:
-            info('executing pattern {}'.format(pattern))
-            execute_pattern(pattern)
-
-        complete_interval()
+    complete_interval()
+    
+#def do_extraction():
+#    
+#    if ramp_rate>0:
+#        '''
+#        style 1.
+#        '''
+#        #               begin_interval(duration)
+#        #               info('ramping to {} at {} {}/s'.format(extract_value, ramp_rate, extract_units)
+#        #               ramp(setpoint=extract_value, rate=ramp_rate)
+#        #               complete_interval()
+#        '''
+#        style 2.
+#        '''
+#        elapsed=ramp(setpoint=extract_value, rate=ramp_rate)
+#        pelapsed=execute_pattern(pattern)
+#        sleep(min(0, duration-elapsed-pelapsed))
+#
+#    else:
+#        begin_interval(duration)
+#        
+#        info('set extract to {} ({})'.format(extract_value, extract_units))
+#        extract()
+#        sleep(2)
+#
+#        if pattern:
+#            info('executing pattern {}'.format(pattern))
+#            execute_pattern(pattern)
+#
+#        complete_interval()
 
 
 #===============================================================================
