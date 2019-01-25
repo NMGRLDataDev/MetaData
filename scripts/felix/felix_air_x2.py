0406b12dc825c6bdd97949af73de12d5002809b4
felix <nmgrluser@gmail.com>
2019-01-25 11:39:13 -0700

    repo updated for analysis a-02-F-1644

--- scripts/felix/felix_air_x2.py
+++ scripts/felix/felix_air_x2.py
@@ -2,30 +2,44 @@
 # EXTRACTION SCRIPT felix_air_x2.py
 #===============================================================================
 '''
-modifier: 01
-eqtime: 30
+modifier: 02
+eqtime: 35
 '''
 
 def main():
-    info("Air Pipette x2")
+    info("Air Pipette x1-4 bone only")
     gosub('felix:WaitForMiniboneAccess')
     gosub('felix:PrepareForAirShot')
     
-    #shot 1
+    #open(name='N')
+    open(name='Q')
+    open(name='D')
+    #open(name='B')
+    
     gosub('common:EvacPipette2')
     gosub('common:FillPipette2')
     gosub('felix:PrepareForAirShotExpansion')
     gosub('common:ExpandPipette2')
+    close(name="Y", description="Outer Pipette 2")
+    sleep(2)
+
+    gosub('common:FillPipette2')
+    gosub('felix:PrepareForAirShotExpansion')
+    gosub('common:ExpandPipette2')
+    close(name="Y", description="Outer Pipette 2")
+    sleep(2)
+
+
+
+    #close(name='B')
+    close(name='Q')
+    close(name='E')
+    #close(name='D')
+    #close(name='N')
     close(description='Outer Pipette 2')
-    sleep(1)
+    sleep(2)
     
-    #shots 2
-    for i in range(1):
-        info('Shot {}'.format(i+2))
-        gosub('common:FillPipette2')
-        gosub('common:ExpandPipette2')
-        close(description='Outer Pipette 2')
-        sleep(1)
+    
 
 #===============================================================================
 # POST EQUILIBRATION SCRIPT felix_pump_extraction_line.py
