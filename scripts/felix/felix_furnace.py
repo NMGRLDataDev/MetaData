45d80b90e25b5555b72b892a656d70f53c64689f
felix <nmgrluser@gmail.com>
2019-03-08 11:22:56 -0700

    repo updated for analysis bu-NF-F-232

--- scripts/felix/felix_furnace.py
+++ scripts/felix/felix_furnace.py
@@ -14,7 +14,7 @@ def main():
     # prepare bone for furnace analysis
     close('F')
     open('D')
-    
+
     # isolate furnace from bone
     close('J')
     close('FC')
@@ -33,71 +33,56 @@ def main():
         '''
         enable()
         # for pi in position:
-#             ''' 
+#             '''
 #             position the laser at pi, pi can be an holenumber or (x,y)
 #             '''
 #             move_to_position(pi)
 #             dump_sample()
-        
+
         do_extraction()
-        
+
         info("Furnace disabled.")
-        
-      
-    #gosub('felix:EquilibrateThenIsolateDiodeColdfinger')    
-    
+
+
+    #gosub('felix:EquilibrateThenIsolateDiodeColdfinger')
+
     # furnace cool down before transfer
     sleep(60)
-    
+
     # prepare bone for transfer
     close('B')
     close('E')
     close('C')
-    
+
     sleep(2)
-    
+
     # equilibrate furnace
     open('J')
     sleep(30)
     close('J')
     open('FC')
-    
+
     sleep(cleanup)
     stop_response_recorder()
 
 def do_extraction():
-    
-    set_pid_parameters(extract_value)
-    
-    if ramp_rate>0:
-        '''
-        style 1.
-        '''
-        #               begin_interval(duration)
-        #               info('ramping to {} at {} {}/s'.format(extract_value, ramp_rate, extract_units)
-        #               ramp(setpoint=extract_value, rate=ramp_rate)
-        #               complete_interval()
-        '''
-        style 2.
-        '''
-        elapsed=ramp(setpoint=extract_value, rate=ramp_rate)
-        pelapsed=execute_pattern(pattern)
-        sleep(min(0, duration-elapsed-pelapsed))
 
+    set_pid_parameters(extract_value)
+
+    begin_interval(duration)
+
+    info('set extract to {} ({})'.format(extract_value, extract_units))
+    extract()
+    sleep(2)
+    complete_interval()
+
+    if extract_value >= 1200:
+        disable()
+    elif extract_value >= 700:
+        extract(400)
     else:
-        begin_interval(duration)
-        
-        info('set extract to {} ({})'.format(extract_value, extract_units))
-        extract()
-        sleep(2)
-        complete_interval()
-        
-        if extract_value >= 1200:
-            disable()
-        elif extract_value >= 700:
-            extract(400)
-        else:
-            extract(300)
+        extract(300)
+
 #===============================================================================
 # POST EQUILIBRATION SCRIPT felix_pump_extraction_line.py
 #===============================================================================
