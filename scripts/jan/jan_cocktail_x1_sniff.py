fb6b443cc4d4369d67cbf8f5b19e89094d89a3c3
nmgrluser <jan@argonlab7s-Mac-mini.local>
2019-06-25 11:15:59 -0600

    repo updated for analysis bc-03-J-98

---
+++ scripts/jan/jan_cocktail_x1_sniff.py
@@ -0,0 +1,45 @@
+#===============================================================================
+# EXTRACTION SCRIPT jan_cocktail_x1_sniff.py
+#===============================================================================
+'''
+modifier: 03
+eqtime: 10
+'''
+def main():
+    info("Jan Cocktail Pipette x1")
+    gosub('jan:WaitForMiniboneAccess')
+    gosub('jan:PrepareForAirShot')
+    gosub('jan:EvacPipette1')
+    gosub('common:FillPipette1')
+    gosub('jan:PrepareForAirShotExpansion')
+    open(name="T", description="Microbone to CO2 Laser")
+    gosub('common:ExpandPipette1')
+    sleep(duration=2.0)
+    close(name="S", description="Microbone to Inlet Pipette")
+#===============================================================================
+# POST EQUILIBRATION SCRIPT jan_pump_extraction_line.py
+#===============================================================================
+def main():
+    info('Pump after analysis')
+
+    if extract_device=="FusionsDiode":
+        info('Pump after Jan diode analysis')
+        gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
+        gosub('jan:PumpMiniboneAfterDiodeAnalysis')
+    else:
+        gosub('jan:PumpMicrobone')
+        v=get_resource_value(name='JanMiniboneFlag')
+        info('get resource value {}'.format(v))
+        if v:
+            info('Pumping Minibone')
+            gosub('jan:PumpMinibone')
+        else:
+            info('Not Pumping Minibone')
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT jan_pump_ms.py
+#===============================================================================
+def main():
+    info('Pumping spectrometer')
+    open(name='O')
+    
