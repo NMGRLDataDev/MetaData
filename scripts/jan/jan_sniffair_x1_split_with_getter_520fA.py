294bf28fde78e8631b54a62560ef955e855ea81f
nmgrluser <jan@argonlab7s-Mac-mini.local>
2019-09-11 17:13:34 -0600

    repo updated for analysis ba-02-J-839

---
+++ scripts/jan/jan_sniffair_x1_split_with_getter_520fA.py
@@ -0,0 +1,45 @@
+#===============================================================================
+# EXTRACTION SCRIPT jan_sniffair_x1_split_with_getter_520fA.py
+#===============================================================================
+'''
+modifier: 02
+eqtime: 10
+'''
+def main():
+    info("Jan Air Sniff Pipette x1")
+    gosub('jan:WaitForMiniboneAccess')
+    gosub('jan:PrepareForAirShot')
+    gosub('jan:EvacPipette2')
+    gosub('common:FillPipette2')
+    gosub('jan:PrepareForAirShotExpansion')
+    close(name="M", description="Microbone to Getter NP-10H")
+    sleep(duration=2.0)
+    gosub('common:SniffPipette2')
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT jan_pump_extraction_line.py
+#===============================================================================
+def main():
+    info('Pump after analysis')
+
+    if extract_device=="FusionsDiode":
+        info('Pump after Jan diode analysis')
+        gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
+        gosub('jan:PumpMiniboneAfterDiodeAnalysis')
+    else:
+        gosub('jan:PumpMicrobone')
+        v=get_resource_value(name='JanMiniboneFlag')
+        info('get resource value {}'.format(v))
+        if v:
+            info('Pumping Minibone')
+            gosub('jan:PumpMinibone')
+        else:
+            info('Not Pumping Minibone')
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT jan_pump_ms.py
+#===============================================================================
+def main():
+    info('Pumping spectrometer')
+    open(name='O')
+    
