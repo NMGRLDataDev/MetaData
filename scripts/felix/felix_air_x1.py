c192bdecdfcf19664de930a470e7f4626ca66ded
felix <nmgrluser@gmail.com>
2020-01-24 16:07:22 -0700

    repo updated for analysis ba-02-F-522

--- scripts/felix/felix_air_x1.py
+++ scripts/felix/felix_air_x1.py
@@ -43,5 +43,5 @@ def main():
 #===============================================================================
 def main():
 	info('Pumping spectrometer')
-	open(name='V')
+	open(name='V', cancel_on_failed_actuation=False)
 	
