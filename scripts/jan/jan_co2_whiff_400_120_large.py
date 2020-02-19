3f6f5c7ea4a181ac15117558b10811bfe155db4f
nmgrluser <jan@argonlab7s-Mac-mini.local>
2020-02-19 08:22:58 -0700

    repo updated for analysis 67285-45

--- scripts/jan/jan_co2_whiff_400_120_large.py
+++ scripts/jan/jan_co2_whiff_400_120_large.py
@@ -36,16 +36,16 @@ whiff:
   abbreviated_count_ratio: 0.25
   conditionals:
     - action: run_remainder
-      teststr: Ar40.cur<=150
+      teststr: Ar40.cur<=140
       attr: Ar40
     - action: run_pipette
       teststr: Ar40.cur>500
       attr: Ar40
     - action: run_chamber_split
-      teststr: Ar40.cur>=250
+      teststr: Ar40.cur>=230
       attr: Ar40
     - action: pump
-      teststr: Ar40.cur>150
+      teststr: Ar40.cur>140
       attr: Ar40
 '''
 
